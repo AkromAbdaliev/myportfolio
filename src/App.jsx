c10ac3280eb54971c344f9ff44e9119d9dfd50ba
Akrom Abdaliev <jacquesalaiyxs@gmail.com>
2024-04-29 23:06:49 +0300

    new images and contact component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import MyWork from "./components/mywork/MyWork";
 import Navbar from "./components/navbar/Navbar";
@@ -13,6 +14,7 @@ const App = () => {
       <About/>
       <Services/>
       <MyWork/>
+      <Contact/>
     </div>
   )
 }
